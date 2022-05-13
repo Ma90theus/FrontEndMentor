457852addaf3903331254befeb0a4fde6ea9c727
Ma90theus <rodrigues.matheusvic@gmail.com>
2022-05-13 19:00:22 -0300

    Removi bloco inválido

--- ARTICLEPREVIEW/main.js
+++
@@ -1,8 +0,0 @@
-function abrirInfo() {
-  document.getElementById('profile').classList.add('info_expanded')
-  console.log('Aberto')
-}
-function fecharInfo() {
-  document.getElementById('profile').classList.remove('info_expanded')
-  console.log('Fechado')
-}
