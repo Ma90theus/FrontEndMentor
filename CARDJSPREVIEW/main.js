4bd5248b4316c5be7995f0254b312722ce7cb2fc
Ma90theus <rodrigues.matheusvic@gmail.com>
2022-05-13 18:57:55 -0300

    Novo Projeto Card com Js

---
+++ CARDJSPREVIEW/main.js
@@ -0,0 +1,8 @@
+function abrirInfo() {
+  document.getElementById('profile').classList.add('info_expanded')
+  console.log('Aberto')
+}
+function fecharInfo() {
+  document.getElementById('profile').classList.remove('info_expanded')
+  console.log('Fechado')
+}
